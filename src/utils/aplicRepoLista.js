c4e717deaa14b0f38f0b8bf011dab7df5da5a465
ernandesferreira <ernandes.dev@gmail.com>
2018-05-04 18:38:14 -0300

    finalizando o front

--- src/utils/aplicRepoLista.js
+++ src/utils/aplicRepoLista.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import axios from 'axios';
 import AplicItem from './aplicRepoItem';
 
 class AplicLista extends React.Component {
+
+    constructor(props) {
+      super(props);
+    }
+
+    
+
     render(){
         return (
             <ul class="list-container">
@@ -10,4 +19,39 @@ class AplicLista extends React.Component {
         );
     }
 }
+
+const mapDispatchToProps = dispatch => {
+  return {
+    loadRepositories : () => dispatch({
+      type: 'REQUEST_REPO',
+      payload: axios.get('https://api.github.com/search/repositories?q=user:globocom&sort=stars:desc&per_page=200')    
+  	}),
+    loadRepoCommits : (item, flag) => {
+      dispatch({
+        type: 'SET_CURRENT_REPO',
+        payload: item
+		  }),
+      dispatch({
+        type: 'REQUEST_COMMITS',
+        payload: axios.get(`https://api.github.com/repos/globocom/${item.name}/commits`, {
+          params: {
+            page: 1,
+            per_page: 20
+          }
+        })
+      }),
+      dispatch({
+        type: 'DISABLE_MENU',
+        payload: flag ? false : true
+      })
+	  }
+  }
+}
+const mapStateToProps = state => {
+  return {
+    repos : state.repos,
+    disableMenu: state.disableMenu
+  }
+}
+
 export default AplicLista
--- src/utils/menu.js
+++ src/utils/menu.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class MenuSidebar extends React.Component {
 	render() {		
